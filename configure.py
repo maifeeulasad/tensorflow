40c642e0fe1da6eb002817d6828c9d0a1184f55b
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-02-19 02:39:43 +0600

    build: added custom version for cuda 12.1 build;

--- configure.py
+++ configure.py
@@ -31,9 +31,9 @@ except ImportError:
   from distutils.spawn import find_executable as which
 # pylint: enable=g-import-not-at-top
 
-_DEFAULT_CUDA_VERSION = '11'
-_DEFAULT_CUDNN_VERSION = '2'
-_DEFAULT_TENSORRT_VERSION = '6'
+_DEFAULT_CUDA_VERSION = '12.1'
+_DEFAULT_CUDNN_VERSION = '8'
+_DEFAULT_TENSORRT_VERSION = '10'
 _DEFAULT_CUDA_COMPUTE_CAPABILITIES = '3.5,7.0'
 
 _SUPPORTED_ANDROID_NDK_VERSIONS = [19, 20, 21, 25]
